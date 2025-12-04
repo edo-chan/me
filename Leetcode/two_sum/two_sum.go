4b603704b8224f872d623b0a0b9dd402782e49b0
Edward Chan <edwardchan@mac.lan>
2025-12-03 20:59:31 -0500

    variables

--- Leetcode/two_sum/two_sum.go
+++ Leetcode/two_sum/two_sum.go
@@ -1,8 +1,8 @@
 package two_sum
 
-func TwoSum(nums []int, target int) []int {
-	for i, num := range nums {
-		for j, num2 := range nums[i:] {
+func TwoSum(input []int, target int) []int {
+	for i, num := range input {
+		for j, num2 := range input[i:] {
 			if num+num2 == target {
 				return []int{i, i + j}
 			}
