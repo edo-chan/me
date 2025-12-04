a4f907ecca20c7ebfed17af5343ddbdc419c913e
Edward Chan <edwardchan@mac.lan>
2025-12-03 20:56:13 -0500

    some leetcode

--- Leetcode/two_sum/two_sum.go
+++ Leetcode/two_sum/two_sum.go
@@ -1,5 +1,13 @@
 package two_sum
 
 func TwoSum(nums []int, target int) []int {
+	for i, num := range nums {
+		for j, num2 := range nums[i:] {
+			if num+num2 == target {
+				return []int{i, i + j}
+			}
+		}
+	}
+
 	return []int{}
 }
--- Leetcode/two_sum/two_sum_test.go
+++ Leetcode/two_sum/two_sum_test.go
@@ -6,7 +6,16 @@ import (
 )
 
 func TestTwoSum(t *testing.T) {
-	nums := []int{2, 7, 11, 15}
-	fmt.Println(nums)
-	_ = nums
+	input := []int{2, 7, 11, 15}
+	fmt.Println(TwoSum(input, 9))
+	fmt.Println(TwoSum(input, 18))
+	fmt.Println(TwoSum(input, 26))
+	fmt.Println(TwoSum(input, 22))
+
+	fmt.Println("negative cases")
+
+	input = []int{-1, -2, -3, 5}
+	fmt.Println(TwoSum(input, -3))
+	fmt.Println(TwoSum(input, -5))
+	fmt.Println(TwoSum(input, 3))
 }
