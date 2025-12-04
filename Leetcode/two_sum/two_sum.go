7879338b411fa39094cb08930e442a73f661ebf7
Edward Chan <edward@chanbros.co>
2025-12-03 21:23:58 -0500

    optimized two sum

--- Leetcode/two_sum/two_sum.go
+++ Leetcode/two_sum/two_sum.go
@@ -11,3 +11,19 @@ func TwoSum(input []int, target int) []int {
 
 	return []int{}
 }
+
+func TwoSumHash(input []int, target int) []int {
+	complements := make(map[int]int)
+	for i, num := range input {
+		complements[num] = i
+	}
+
+	for i, num := range input {
+		complement := target - num
+		if firstIndex := complements[complement]; firstIndex != 0 {
+			return []int{i, firstIndex}
+		}
+	}
+
+	return []int{}
+}
--- Leetcode/two_sum/two_sum_test.go
+++ Leetcode/two_sum/two_sum_test.go
@@ -19,3 +19,18 @@ func TestTwoSum(t *testing.T) {
 	fmt.Println(TwoSum(input, -5))
 	fmt.Println(TwoSum(input, 3))
 }
+
+func TestTwoSumHash(t *testing.T) {
+	input := []int{2, 7, 11, 15}
+	fmt.Println(TwoSumHash(input, 9))
+	fmt.Println(TwoSumHash(input, 18))
+	fmt.Println(TwoSumHash(input, 26))
+	fmt.Println(TwoSumHash(input, 22))
+
+	fmt.Println("negative cases")
+
+	input = []int{-1, -2, -3, 5}
+	fmt.Println(TwoSumHash(input, -3))
+	fmt.Println(TwoSumHash(input, -5))
+	fmt.Println(TwoSumHash(input, 3))
+}
